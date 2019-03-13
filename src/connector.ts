c3a5778c03e0e7115b5bce2758ea244ada94c9e4
Ángel González <aglezabad@gmail.com>
2019-03-13 22:51:21 +0100

    Added draft of method for api get functions.

--- src/connector.ts
+++ src/connector.ts
@@ -7,9 +7,9 @@ export class TadoConnector extends Connector {
 		super("Tado");
 	}
 
-	addConnection(options : any, callback : Function): void {
+	addConnection(options: any, callback: Function): void {
 		const self = this;
-		const connection : TadoConnection = new TadoConnection(options);
+		const connection: TadoConnection = new TadoConnection(options);
 
 		connection.getClient()
 		.login(connection.getUsername(), connection.getPassword())
@@ -17,14 +17,14 @@ export class TadoConnector extends Connector {
 			self.connections.push(connection);
 			callback(null, connection.getId());
 		})
-		.catch(function(err : Error) {
+		.catch(function(err: Error) {
 			callback(err);
 		});
 	}
 
-	getMe(id : string, options : any = {}, callback : Function) : void {
+	getMe(id: string, options: any = {}, callback: Function): void {
 		const self = this;
-		const connection : TadoConnection = <TadoConnection> self.getConnection(id);
+		const connection: TadoConnection = <TadoConnection> self.getConnection(id);
 		if (connection) {
 			connection.getClient().getMe().then(function(response: any) {
 				callback(null, response);
@@ -35,26 +35,47 @@ export class TadoConnector extends Connector {
 		}
 	}
 
-	receiveMessage(id : string, options : any = {}, callback : Function) : void {
+	receiveMessage(id: string, options: any = {}, callback: Function): void {
 		const self = this;
-		const connection : TadoConnection = <TadoConnection> self.getConnection(id);
-		if (connection) {
+		const connection: TadoConnection = <TadoConnection> self.getConnection(id);
+		const optionsError: Error = self.verifyReceiveMessageBaseOptions(options);
+		if (connection && !optionsError) {
+			self.callHttpApiGetMethod(connection, options, function(err: Error, response: any) {
 
+			});
+		} else if (!connection) {
+			callback(new Error("No connection on list with id: " + id));
+		} else {
+			callback(optionsError);
 		}
 	}
 
-	sendMessage(id : string, options : any = {}, callback : Function) : void {
-		callback(new Error("Not implemented"));
+	private verifyReceiveMessageBaseOptions(options: any): Error {
+		let error: Error
+		if (!options.api_method && !options.home_id) {
+			error = new Error("Parameters api_method and home_id are required in Tado API.");
+		} else {
+			error = null;
+		}
+		return error;
+	}
+
+	private callHttpApiGetMethod(connection: TadoConnection, options: any, callback: Function): void {
+		switch options.api_method
+	}
+
+	sendMessage(id: string, options: any = {}, callback: Function): void {
+		callback(new Error("Not implemented yet"));
 	}
 
 }
 
 export class TadoConnection extends Connection {
-	private username : string;
-	private password : string;
-	private tadoClient : any;
+	private username: string;
+	private password: string;
+	private tadoClient: any;
 
-	constructor (options: any) {
+	constructor(options: any) {
 		super(options);
 		this.username = options.username;
 		this.password = options.password;
@@ -69,7 +90,7 @@ export class TadoConnection extends Connection {
 		return this.password;
 	}
 
-	getClient() : any {
+	getClient(): any {
 		return this.tadoClient;
 	}
 }
